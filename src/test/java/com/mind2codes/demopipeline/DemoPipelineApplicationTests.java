7fddebc279abd21e21f75044a4b9728493bf5640
Ghislain ETIEN <mac_user@MacBook-Pro-de-mac-user.local>
2019-11-10 13:33:31 +0000

    first commit

---
+++ src/test/java/com/mind2codes/demopipeline/DemoPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mind2codes.demopipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
